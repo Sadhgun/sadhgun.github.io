b68a2cad4870ccb2002997fee682351ad21e55e0
Sadhgun <sadhgunsobti@gmail.com>
2020-05-26 20:04:51 +0530

    Basic Changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Sadhgun Sobti`,
+    description: `Personal website of Sadhgun Sobti.`,
+    author: `@SadhgunSobti`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
