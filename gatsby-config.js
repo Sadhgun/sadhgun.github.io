0fd12db95a581d4940747d23cb55e708ad20f93a
Sadhgun <sadhgunsobti@gmail.com>
2020-05-29 22:07:29 +0530

    Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,8 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-167973792-1",
-      },
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
